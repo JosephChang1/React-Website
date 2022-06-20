0415b835949212636b0c64bbeca36f4a1731cba6
Joseph Chang <joseph123749@gmail.com>
2022-06-20 13:44:56 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import styled from "styled-components";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import logo from './logo.png'
+/*
+const theme = {
+    blue: {
+      default: "#3f51b5",
+      hover: "#283593"
+    },
+    pink: {
+      default: "#e91e63",
+      hover: "#ad1457"
+    }
+  };
+
+const Button = styled.button`
+  background-color: ${(props) => theme[props.theme].default};
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: ${(props) => theme[props.theme].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+*/
+
+function Welcome(props) {
+    return <h2>Hello, {props.name}</h2>;
+}
+
+function clickMe(){
+    alert('ok stop');
+    
+}
+function App() {
+    return (
+      <div>
+         
+        <div>
+          <img className='logo' src={logo} alt = 'doge' size='100px'/>
+          <h1> Bhutan-Taiwan Assiciation </h1>
+        </div>
+        
+        <div>
+            <button onClick={clickMe}>
+                Don't Press
+            </button>
+        </div>
+
+        <div>
+          <h1 style={{color: "Grey", backgroundColor: "Blue", width: '4.5rem', alignContent:'left'}}> 
+          ok 
+          </h1>
+        </div>
+        
+      </div>
+    );
+  }
+//const element = <Welcome name='Sara'/>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ root.render(<App/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
